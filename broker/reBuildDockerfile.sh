5245a34da74a0f3a3dca57c3515ba6e8a4574b09
fabiocingottini <fabio.cingottini@gmail.com>
2018-11-18 19:57:34 +0100

    consolidated broker dockerization

--- broker/reBuildDockerfile.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-sudo docker rmi mqtt:latest
-sudo docker build -f Dockerfile.mqtt_docker_broker -t mqtt_docker_broker:latest .
--- broker/startMqttBroker.sh
+++ broker/startMqttBroker.sh
@@ -7,5 +7,7 @@ sudo docker run \
     -d \
     --name mqtt_docker_broker \
     --network 526399fefd15 \
-    -v $(pwd)/mosquitto.log:/mosquitto/log/mosquitto.log \
-    mqtt_docker_broker:latest
+    -u 996:995 \
+    -v $(pwd)/log:/mosquitto/log:rw \
+    -v $(pwd)/config/mosquitto.config:/mosquitto/config/mosquitto.config:ro \
+    eclipse-mosquitto:1.5.4
